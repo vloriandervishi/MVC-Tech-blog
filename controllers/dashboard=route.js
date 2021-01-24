35f6d3fede6e74f42c37d6e0f9c38dbd2ef76183
Valdo <vlorian22@GMAIL.COM>
2021-01-23 20:17:43 -0600

    working on dashboard routes and views dashboard

--- controllers/dashboard=route.js
+++ controllers/dashboard=route.js
@@ -3,6 +3,37 @@ const sequelize= require('../config/connection');
 const {Post, User, Comment}= require('../models');
 
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
+   // SELECT *FROM id,title,content,created from user_id
+    Post.findAll({
+      where: {
+        user_id:req.session.user_id
+      },
+      attributes:[
+        'id',
+        'title',
+        'content',
+        'created_at'
+      ],
+      include: [
+        {
+          model:Comment,
+          attributes:['id', 'comment_text','post_id', 'user_id', 'created_id'],
+          include:{
+            model:User,
+            attributes:['username']
+          }
+        },
+        {
+          model:User,
+          attributes:['username']
+        }
+      ]
+    }).then(dbPostData=>{
+      const posts= dbPostData.map(post=> post.get({plain:true}));
+      res.render('dashboard',{posts,loggedIn:true});
+    }).catch(error=>{
+      console.log(error);
+      res.status(500).json(err);
+    })
   });
 module.exports=router;
--- models/index.js
+++ models/index.js
@@ -1,67 +1,67 @@
-// // import all models
-// const Post = require('./Post');
-// const User = require('./User');
+// import all models
+const Post = require('./Post');
+const User = require('./User');
 
-// const Comment = require('./Comment');
+const Comment = require('./Comment');
 
-// // create associations
-// User.hasMany(Post, {
-//   foreignKey: 'user_id'
-// });
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
-// Post.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// User.belongsToMany(Post, {
+User.belongsToMany(Post, {
+ // through: Vote,
+  as: 'voted_posts',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+// Post.belongsToMany(User, {
 //  // through: Vote,
-//   as: 'voted_posts',
-
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
-
-// // Post.belongsToMany(User, {
-// //  // through: Vote,
-// //   //as://'voted_posts',
-// //   foreignKey: 'post_id',
-// //   onDelete: 'SET NULL'
-// // });
-
-// Vote.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
-
-// Vote.belongsTo(Post, {
+//   //as://'voted_posts',
 //   foreignKey: 'post_id',
 //   onDelete: 'SET NULL'
 // });
 
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
 
 
-// Post.hasMany(Vote, {
-//   foreignKey: 'post_id'
-// });
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
 
-// Comment.belongsTo(Post, {
-//   foreignKey: 'post_id',
-//   onDelete: 'SET NULL'
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// User.hasMany(Comment, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
 
-// Post.hasMany(Comment, {
-//   foreignKey: 'post_id'
-// });
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
-// module.exports = { User, Post, Vote, Comment };
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
