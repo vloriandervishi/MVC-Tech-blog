a27a00dfb4e92956d72f9d36a0c5391702d0f050
Valdo <vlorian22@GMAIL.COM>
2021-01-23 21:02:28 -0600

    worked on hooking front end to the back end usin javascript

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const homeRoute= require('./home-route');
-const dashboardRoutes = require('./dashboard=route');
+const dashboardRoutes = require('./dashboard-route');
 
 router.use('/dashboard', dashboardRoutes);
 router.use('/',homeRoute);
+router.use('/api', apiRoutes);
 
 module.exports=router;
